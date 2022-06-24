0c05f50d0b7ee684f13822c6d73cf256e07e38cd
lyyuwen <coldnatural@outlook.com>
2022-06-24 18:45:02 +0800

    news

---
+++ harmonyos-version/entry/src/main/java/com/application/easyteaching/MyApplication.java
@@ -0,0 +1,10 @@
+package com.application.easyteaching;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
